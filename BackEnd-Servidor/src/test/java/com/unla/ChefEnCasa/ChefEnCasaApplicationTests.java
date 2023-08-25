ae948af4f6376dcc4c7576dfcce05d8039f0b593
Francisco Armendariz <franciscoarmendarizlaboral@gmail.com>
2023-08-24 21:01:34 -0300

    initial commit

---
+++ BackEnd-Servidor/src/test/java/com/unla/ChefEnCasa/ChefEnCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.ChefEnCasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefEnCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
