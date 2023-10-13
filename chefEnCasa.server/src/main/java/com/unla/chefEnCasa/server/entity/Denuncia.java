5d65b27c0a0b131e71cd57745211aa7b7abfaee1
Franco Ponce <francoponce20@hotmail.com>
2023-10-13 18:11:15 -0300

    entidad denuncia

---
+++ chefEnCasa.server/src/main/java/com/unla/chefEnCasa/server/entity/Denuncia.java
@@ -0,0 +1,32 @@
+package com.unla.chefEnCasa.server.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Denuncia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String estado; // NUEVO | RESULTA
+    private String motivo; // "CONTENIDO INAPROPIADO" | "INGREDIENTES PROHIBIDOS" | "PELIGROSO PARA LA SALUD"
+
+    @ManyToOne
+    @JoinColumn(name = "receta_id")
+    private Receta receta;
+}
