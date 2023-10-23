e8d67e089813a5e4c50f5e65230de042dcff2e4c
unknown <fiaschenicolas@gmail.com>
2023-10-22 23:17:26 -0300

    commit

--- chefEnCasa.server/src/main/java/com/unla/chefEnCasa/server/Soap/endpoint/MensajeEndPoint.java
+++ chefEnCasa.server/src/main/java/com/unla/chefEnCasa/server/Soap/endpoint/MensajeEndPoint.java
@@ -3,6 +3,7 @@ package com.unla.chefEnCasa.server.Soap.endpoint;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -40,9 +41,11 @@ public class MensajeEndPoint {
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "postMensajeRequest")
     @ResponsePayload
+    @Async
     public PostMensajeResponse crearMensaje(@RequestPayload PostMensajeRequest request) {
         PostMensajeResponse response=new PostMensajeResponse();
-        System.out.println(request);  
+        System.out.println("postMensajeRequest");
+        System.out.println(request);
         mensajeService.CrearMensaje(request);
 
         return response;
@@ -50,8 +53,10 @@ public class MensajeEndPoint {
     
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "postMensajeRespuestaRequest")
     @ResponsePayload
+    @Async
     public PostMensajeRespuestaResponse responderMensaje(@RequestPayload PostMensajeRespuestaRequest request) {
         PostMensajeRespuestaResponse response= new PostMensajeRespuestaResponse();
+        System.out.println("responderMensaje");  
         MensajeModel mensaje=mensajeRepository.findById(request.getMensajeId()).orElse(null);
         mensaje.setRespuesta(request.getRespuesta());
         mensajeRepository.save(mensaje);
@@ -63,6 +68,7 @@ public class MensajeEndPoint {
     
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getTraerMensajePorAutorRequest")
     @ResponsePayload
+    @Async
     public GetTraerMensajeResponse traerMensajesPorAutor(@RequestPayload GetTraerMensajePorAutorRequest request) {
          GetTraerMensajeResponse response= new GetTraerMensajeResponse();
         List<MensajeModel> mensaje= mensajeService.traerMensajeModelsPorAutor(request.getIdAutor());
@@ -75,6 +81,7 @@ public class MensajeEndPoint {
        
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getTraerMensajePorReceptorRequest")
     @ResponsePayload
+    @Async
     public GetTraerMensajeResponse traerMensajesPorRecetor(@RequestPayload GetTraerMensajePorReceptorRequest request) {
          GetTraerMensajeResponse response= new GetTraerMensajeResponse();
         List<MensajeModel> mensaje= mensajeService.traerMensajeModelsPorReceptor(request.getIdReceptor());
